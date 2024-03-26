d68e906e9d439c2d5bfbb25456ac6cf0a2cf86d2
Ezequiel <ezealba2@gmail.com>
2024-03-25 22:13:16 -0300

    w04

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -7,6 +7,7 @@ let myProfile =
     photo: "images/eze.png.jpg",
     favoriteFoods: ['Pizza','Burguer','Asado'],
     hobbies: ['Surf','Programming Arduino','Watch'],
+    placesLived :[]
 };
 
 
@@ -20,9 +21,46 @@ myProfile.placesLived.push(
         length: '20 years'
     }
 );
-
+myProfile.placesLived.push(
+    {
+        place: 'Miramar, BA',
+        length: '3 years'
+    }
+);
+myProfile.placesLived.push(
+    {
+        place: 'Lima, PERU',
+        length: '2 years'
+    }
+);
 
 /* DOM Manipulation - Output */
+document.querySelector('#name').textContent = myProfile.name;
+document.querySelector('#photo').setAttribute('src', myProfile.photo);
+document.querySelector('#photo').setAttribute('alt', myProfile.name);
+
+myProfile.favoriteFoods.forEach(food => {
+  let li = document.createElement('li');
+  li.textContent = food;
+  document.querySelector('#favorite-foods').appendChild(li);
+});
+
+myProfile.hobbies.forEach(hobby => {
+    let li = document.createElement('li');
+    li.textContent = hobby;
+    document.querySelector('#hobbies').appendChild(li);
+  });
+
+myProfile.placesLived.forEach(places => {
+    let dt = document.createElement('dt');
+    let dd = document.createElement('dd');
+    dt.textContent = places.place;
+    dd.textContent = places.length;
+
+    document.querySelector('#places-lived').appendChild(dt);
+    document.querySelector('#places-lived').appendChild(dd);
+  });  
+
 
 /* Name */
 
