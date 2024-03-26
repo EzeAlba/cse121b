58dbaeaf0bc96385d40643d833d96ee8ee8c106a
Ezequiel <ezealba2@gmail.com>
2024-03-25 21:29:42 -0300

    Restructure and W05 Task

---
+++ scripts/w04-task.js
@@ -0,0 +1,40 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = 
+{
+    name: "Ezequiel Albarracin",
+    photo: "images/eze.png.jpg",
+    favoriteFoods: ['Pizza','Burguer','Asado'],
+    hobbies: ['Surf','Programming Arduino','Watch'],
+};
+
+
+
+
+/* Populate Profile Object with placesLive objects */
+
+myProfile.placesLived.push(
+    {
+        place: 'Mar del Plata, BA',
+        length: '20 years'
+    }
+);
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
